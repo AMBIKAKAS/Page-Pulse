eea5487ecb48e5ec65f51f48004bc0ad1bd6998f
ambikakashyap736 <ambikakashyap736@gmail.com>
2026-07-25 16:41:09 +0530

    Updated README and deployment changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"http://localhost:8000/api",
+    baseURL: "https://page-pulse-backend-t6np.onrender.com/",
     timeout:10000
 });
