a90d0db44c1bda88c31ea82f9f5c913d4f94eb7c
ambikakashyap736 <ambikakashyap736@gmail.com>
2026-07-25 17:02:27 +0530

    push

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://page-pulse-backend-t6np.onrender.com/",
-    timeout:10000
+  baseURL: "https://page-pulse-backend-t6np.onrender.com/api",
+  timeout: 10000,
 });
